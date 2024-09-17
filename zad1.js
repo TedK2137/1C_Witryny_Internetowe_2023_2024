b6b3a4e6c16e28bb703946b8375e4473f5f4fa13
TedK2137 <144451113+TedK2137@users.noreply.github.com>
2024-09-17 14:58:04 +0200

    zad1

---
+++ zad1.js
@@ -0,0 +1,17 @@
+var liczba1 = prompt("Podaj liczbę");
+var liczba2 = prompt("podaj drugą liczbę");
+
+var dodawanie = liczba1 + liczba2;
+document.write(dodawanie);
+document.write("<br></br>");
+
+var odejmowanie = liczba1 - liczba2;
+document.write(odejmowanie);
+document.write("<br></br>");
+
+var dzielenie = liczba1 / liczba2;
+document.write(dzielenie);
+document.write("<br></br>");
+
+var mnozenie = liczba1 * liczba2;
+document.write(mnozenie);
